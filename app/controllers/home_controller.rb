c004109746bb78f13529ea53e391fc100caae195
leechungrae <leechungrae@gmail.com>
2016-07-28 13:34:21 +0900

    relate pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
   end
+  def about
+  end
+  def pricing
+  end
+  def tour
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   root 'home#index'
+  get '/tour' => 'home#tour'
+  get '/about' => 'home#about'
+  get '/pricing' => 'home#pricing'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
